36d63894314660e4570c3406562375a6a27533b0
Ashish Singh <ashish.ksingh8765@gmail.com>
2024-02-25 18:35:26 +0530

    loader size updated

--- app/page.tsx
+++ app/page.tsx
@@ -80,8 +80,8 @@ export default function Home() {
                 <Image
                   src="/car-loader.gif"
                   alt="loader"
-                  width={50}
-                  height={50}
+                  width={100}
+                  height={100}
                   className="object-cover"
                 />
               </div>
